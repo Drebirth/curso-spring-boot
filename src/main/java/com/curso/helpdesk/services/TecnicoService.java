f67c1508862b6e7e5ac3842ed39dfd571c242a86
drebirth <danielll98@outlook.com.br>
2022-08-14 16:34:49 -0300

    excecao para objeto nao encontrado

--- src/main/java/com/curso/helpdesk/services/TecnicoService.java
+++ src/main/java/com/curso/helpdesk/services/TecnicoService.java
@@ -1,11 +1,11 @@
 package com.curso.helpdesk.services;
 
 import com.curso.helpdesk.domain.Tecnico;
+import com.curso.helpdesk.exceptions.ObjectNotFoundException;
 import com.curso.helpdesk.repository.TecnicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,7 +16,8 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!" + id));
+
     }
 
 

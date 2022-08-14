c146de87d97dc75d7c5020e74835579ddb98c17d
drebirth <danielll98@outlook.com.br>
2022-08-14 15:54:43 -0300

    endpoint findById para tecnico

--- src/main/java/com/curso/helpdesk/services/TecnicoService.java
+++ src/main/java/com/curso/helpdesk/services/TecnicoService.java
@@ -1,2 +1,24 @@
-package com.curso.helpdesk.services;public class TecnicoService {
+package com.curso.helpdesk.services;
+
+import com.curso.helpdesk.domain.Tecnico;
+import com.curso.helpdesk.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TecnicoService {
+
+    @Autowired
+    private TecnicoRepository repository;
+
+    public Tecnico findById(Integer id){
+        Optional<Tecnico> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+
+
 }

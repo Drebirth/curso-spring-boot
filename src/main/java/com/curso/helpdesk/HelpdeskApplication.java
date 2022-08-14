147115b5b95d46ad56e744f62fad8278f9dead0b
drebirth <danielll98@outlook.com.br>
2022-08-14 15:07:29 -0300

    carga inicial da base de dados

--- src/main/java/com/curso/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/curso/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,55 @@
 package com.curso.helpdesk;
 
+import com.curso.helpdesk.domain.Chamado;
+import com.curso.helpdesk.domain.Cliente;
+import com.curso.helpdesk.domain.Tecnico;
+import com.curso.helpdesk.domain.enums.Perfil;
+import com.curso.helpdesk.domain.enums.Prioridade;
+import com.curso.helpdesk.domain.enums.Status;
+import com.curso.helpdesk.repository.ChamadoRepository;
+import com.curso.helpdesk.repository.ClienteRepository;
+import com.curso.helpdesk.repository.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class HelpdeskApplication {
+public class HelpdeskApplication implements CommandLineRunner{
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private ChamadoRepository chamadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Tecnico tec1 = new Tecnico(null,"Daniel","72314306201","daniel@hotmail.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+
+		Cliente cli1 = new Cliente(null,"Linus Torvalds", "12345678901","torvalds@email.com", "123");
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO,"chmado 01","Primeiro chamado", tec1, cli1);
+
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+
+		System.out.println("TECNICO" +tecnicoRepository.findAll());
+		System.out.println("CLIENTE" +clienteRepository.findAll());
+		System.out.println("CHAMADO" +chamadoRepository.findAll());
+
+
+	}
 }

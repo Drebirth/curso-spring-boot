d83325996320ca84e269a1c106bc06a8150cfb20
drebirth <danielll98@outlook.com.br>
2022-08-16 21:43:08 -0300

    criado chamadoDTO, chamado Service, chamado resource

---
+++ src/main/java/com/curso/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.curso.helpdesk.services;
+
+import com.curso.helpdesk.domain.Chamado;
+import com.curso.helpdesk.repository.ChamadoRepository;
+import com.curso.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id){
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Chamado não encontrado" + id));
+    }
+
+}
